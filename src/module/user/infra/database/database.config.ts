2c34710fa74094fe1aad1b1818420f8d232fb5e7
Manoel Neto <manoel.neto@ingaia.com.br>
2022-05-23 02:16:57 -0300

    confi postgres heroku

--- src/module/user/infra/database/database.config.ts
+++ src/module/user/infra/database/database.config.ts
@@ -2,10 +2,12 @@ import { config } from 'dotenv';
 config({ path: '.env' });
 export default {  
     conn_type: String(process.env.CONN_TYPEORM_TYPE),
+    conn_database_url: process.env.DATABASE_URL,
     conn_host: process.env.CONN_TYPEORM_HOST,
     conn_database: process.env.CONN_TYPEORM_DATABASE,
     conn_username: process.env.CONN_TYPEORM_USERNAME,
     conn_password: process.env.CONN_TYPEORM_PASSWORD,
-    conn_port: parseInt(process.env.CONN_TYPEORM_PORT),  
+    conn_port: parseInt(process.env.CONN_TYPEORM_PORT),
+
  
 };
--- src/module/user/infra/database/database.providers.ts
+++ src/module/user/infra/database/database.providers.ts
@@ -9,13 +9,19 @@ export const databaseProviders = [
     provide: 'DATABASE_CONNECTION',
     useFactory: async () =>
       await createConnection({
+        
         type: 'postgres',
-        host: database.conn_host,
-        port: database.conn_port,
-        username: database.conn_username,
-        password: database.conn_password,
-        database: database.conn_database,
-        entities: [path.join(__dirname, '../../**/', '*.entity.{ts,js}')],           
+        url:database.conn_database_url,
+        // host: database.conn_host,
+        // port: database.conn_port,
+        // username: database.conn_username,
+        // password: database.conn_password,
+        // database: database.conn_database,
+        entities: [path.join(__dirname, '../../**/', '*.entity.{ts,js}')],  
+        ssl:{
+          rejectUnauthorized: false}
+        
+                 
       }),
   },
 ];
