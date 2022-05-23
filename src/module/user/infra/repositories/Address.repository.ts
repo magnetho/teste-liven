7586baacb7babc94e429a761fcc4ca871802e641
Manoel Neto <manoel.neto@ingaia.com.br>
2022-05-23 00:51:03 -0300

    change file

--- src/module/user/infra/repositories/Address.repository.ts
+++ src/module/user/infra/repositories/Address.repository.ts
@@ -1,5 +1,4 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Address } from '../../domain/entities';
 
