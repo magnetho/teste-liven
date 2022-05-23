bde8254935961ccffcc6338f356326e5fc7d9dd9
Manoel Neto <manoel.neto@ingaia.com.br>
2022-05-23 02:24:54 -0300

    change config dabaseConnection

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,13 @@
 module.exports = {
    "type": "postgres",
-   "host": process.env.CONN_TYPEORM_HOST,
-   "port": process.env.CONN_TYPEORM_PORT,
-   "username": process.env.CONN_TYPEORM_USERNAME,
-   "password": process.env.CONN_TYPEORM_PASSWORD,
-   "database": process.env.CONN_TYPEORM_DATABASE,
+   "url":process.env.DATABASE_URL,
+   "ssl":{rejectUnauthorized: false},
+
+   // "host": process.env.CONN_TYPEORM_HOST,
+   // "port": process.env.CONN_TYPEORM_PORT,
+   // "username": process.env.CONN_TYPEORM_USERNAME,
+   // "password": process.env.CONN_TYPEORM_PASSWORD,
+   // "database": process.env.CONN_TYPEORM_DATABASE,
    "entities": [
       "dist/**/*.entity.js"
    ],
